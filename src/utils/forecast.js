bfb7a38fda196fa9d0a825d534ce9d1c32c49d98
David Reed <technologychameleon@gmail.com>
2023-07-08 13:48:18 -0500

    Initial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,17 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=dd9426cf8c12afc760a16d5855815a07&query=' + latitude + ',' + longitude + '&units=f'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location', undefined)
+        } else {
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees')
+        }
+    })
+}
+
+module.exports = forecast
