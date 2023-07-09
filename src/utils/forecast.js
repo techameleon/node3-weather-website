5e073ea639b771938c2f3e01f234bcb1d3e8301f
David Reed <technologychameleon@gmail.com>
2023-07-08 19:14:23 -0500

    added humidit

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees')
+            callback(undefined, body.current.weather_descriptions[0] + ' It is currently ' + body.current.temperature + ' degress out. It feels like ' + body.current.feelslike + ' degrees.  The humidity is ' + body.current.humidity + '%.')
         }
     })
 }
